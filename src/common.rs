6ee1042629ef8f13163563a3d2e4118f64ba7590
zzx-202506 <1026948656@qq.com>
2025-06-13 18:27:27 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://222.82.211.74".to_owned()
 }
 
 #[inline]
