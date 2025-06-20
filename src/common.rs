925588196364596ef208971f3dc79849bd542c4f
zzx-202506 <1026948656@qq.com>
2025-06-20 15:49:53 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://zzxvip.uicp.top".to_owned()
 }
 
 #[inline]
